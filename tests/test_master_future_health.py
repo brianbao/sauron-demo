eea2a12648b4cfa06fdfb7279800cafc255efed9
One Bot To Merge Them All (in gamma) <sauron+gamma@zenefits.com>
2018-03-05 16:03:28 -0800

    add freeze_time to fix tests

--- tests/test_master_future_health.py
+++ tests/test_master_future_health.py
@@ -1,4 +1,11 @@
+from __future__ import absolute_import
+
+# Put Pip Imports Here:
+from freezegun import freeze_time
+
+
 class Foo:
+    @freeze_time('2017-12-31')
     def bar(self):
         pass
         
